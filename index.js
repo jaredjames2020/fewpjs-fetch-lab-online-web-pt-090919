48d9030b4130df5ddd3ff52e820e29a451c863a4
Jared S <jaredjames2020@gmail.com>
2020-04-01 17:21:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function fetchBooks() {
-<<<<<<< HEAD
   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json));
-=======
- 
->>>>>>> c0de5f2a0b1a7fcdfb4e2b240c8088288e9ce235
 }
 
 function renderBooks(json) {
